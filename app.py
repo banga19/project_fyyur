283d058b06aa2447fa5a711dd08ca9f6effebe94
banga19 <bangalifof16@gmail.com>
2025-05-30 12:57:56 +0300

    fixed bugs

--- app.py
+++ app.py
@@ -427,14 +427,26 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
-
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+  form = ArtistForm(request.form)
+  try:
+    new_artist = Artist(
+      name=form.name.data,
+      city=form.city.data,
+      state=form.state.data,
+      phone=form.phone.data,
+      genres=','.join(form.genres.data) if hasattr(form.genres, 'data') and isinstance(form.genres.data, list) else form.genres.data,
+      image_link=form.image_link.data,
+      facebook_link=form.facebook_link.data
+    )
+    db.session.add(new_artist)
+    db.session.commit()
+    flash('Artist ' + new_artist.name + ' was successfully listed!')
+  except Exception as e:
+    db.session.rollback()
+    flash('An error occurred. Artist could not be listed.')
+    print(f"Error adding artist: {e}")
+  finally:
+    db.session.close()
   return render_template('pages/home.html')
 
 
